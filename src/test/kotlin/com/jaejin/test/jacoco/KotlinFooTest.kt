09bd304e29617cd309b13116ac2e52e511dedb36
simjjy89 <simjjy89@gmail.com>
2024-02-28 17:23:05 +0900

    최초 커밋

---
+++ src/test/kotlin/com/jaejin/test/jacoco/KotlinFooTest.kt
@@ -0,0 +1,17 @@
+package com.jaejin.test.jacoco
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+
+internal class KotlinFooTest {
+
+    private val kotlinFoo = KotlinFoo()
+
+    @Test
+    fun `partially covered hello method test`() {
+        val actual = kotlinFoo.hello("펭")
+        assertEquals(actual, "하")
+    }
+
+}
